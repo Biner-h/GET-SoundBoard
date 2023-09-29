56dbeaeb78aa88de61228e388c88da785a23eefd
Biner-h <Binerhabib@gmail.com>
2023-09-29 14:31:02 +0200

    update

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 let app = document.getElementById("app");
-
+let app1 = document.querySelector(".dapp");
 updateView();
 function updateView() {
     app.innerHTML = /*html*/ `
     <div class="container">
-        <div id="bubbles" class="square" onclick="playSound('bubbles')">
-            <audio class="sound" src="./sounds/bubbles.mp3"></audio>
-        </div>
-        <div id="moon" class="square" onclick="playSound('moon')">
-            <audio class="sound" src="./sounds/moon.mp3"></audio>
-        </div>
-        <div id="confetti" class="square" onclick="playSound('confetti')">
-            <audio class="sound" src="./sounds/confetti.mp3"></audio>
-        </div>
-        <div id="ufo" class="square" onclick="playSound('ufo')">
-            <audio class="sound" src="./sounds/ufo.mp3"></audio>
-        </div>
-        <div id="glimmer" class="square" onclick="playSound('glimmer')">
-            <audio class="sound" src="./sounds/glimmer.mp3"></audio>
-        </div>
+        <div class="square" onclick="playSound('bubbles.mp3')"></div>
+        <div class="square" onclick="playSound('confetti.mp3')"></div>
+        <div class="square" onclick="playSound('glimmer.mp3')"></div>
+        <div class="square" onclick="playSound('moon.mp3')"></div>
+        <div class="square" onclick="playSound('ufo.mp3')"></div>
     </div>
     `;
 }
 
-function playSound(selectedSound) {
-    document.getElementById(selectedSound).playSound();
-}
+const playSound = (name) => {
+    const path = "./audio/" + name;
+    app1.innerHTML = `<audio src="${path}" autoplay></audio>`;
+};
